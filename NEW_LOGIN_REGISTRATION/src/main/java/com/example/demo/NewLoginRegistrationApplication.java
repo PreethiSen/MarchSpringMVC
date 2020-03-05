d1a3f70956f8eaa26a9ae4fdc107ce89ed415387
PreethiSen <58634090+PreethiSen@users.noreply.github.com>
2020-03-05 12:48:27 -0500

    Add files via upload

---
+++ NEW_LOGIN_REGISTRATION/src/main/java/com/example/demo/NewLoginRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewLoginRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewLoginRegistrationApplication.class, args);
+	}
+
+}
